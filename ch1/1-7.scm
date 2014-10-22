17fac7caa4cfbc4a847918cc76d329e95b4c3333
yksz <user4git@gmail.com>
2014-10-22 14:34:29 +0900

    Add 1-7.scm

---
+++ ch1/1-7.scm
@@ -0,0 +1,25 @@
+(define (sqrt-iter guess prev-guess x)
+  (if (good-enough? guess prev-guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 guess
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess prev-guess x)
+  (< (abs (- guess prev-guess)) 0.001))
+
+(define (square x)
+  (* x x))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 0.0 x))
+
+
+(require "../util/assert")
+(assert-approximate 0.01 (sqrt 0.0001) 0.001 "(sqrt 0.0001)")
--- util/assert.scm
+++ util/assert.scm
@@ -1,3 +1,8 @@
 (define (assert expr msg)
   (if (not expr)
       (print (string-append "Assertion Failed: " msg))))
+
+(define (assert-approximate expected expr error msg)
+  (assert (and (>= expr (- expected error))
+               (<= expr (+ expected error)))
+          msg))
