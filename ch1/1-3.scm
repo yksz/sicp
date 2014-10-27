47063c9834357013d63fa5731df806234208cc3f
yksz <user4git@gmail.com>
2014-10-27 09:41:11 +0900

    Refactor 1-3.scm

--- ch1/1-3.scm
+++ ch1/1-3.scm
@@ -1,4 +1,4 @@
-(define (fn x y z)
+(define (f x y z)
   (sum-of-square (max x y)
                  (max (min x y) z)))
 
@@ -10,9 +10,9 @@
 
 
 (require "../util/assert")
-(assert-eqv "(fn 1 2 3)" 13 (fn 1 2 3))
-(assert-eqv "(fn 1 3 2)" 13 (fn 1 3 2))
-(assert-eqv "(fn 2 1 3)" 13 (fn 2 1 3))
-(assert-eqv "(fn 2 3 1)" 13 (fn 2 3 1))
-(assert-eqv "(fn 3 1 2)" 13 (fn 3 1 2))
-(assert-eqv "(fn 3 2 1)" 13 (fn 3 2 1))
+(assert-eqv "(f 1 2 3)" 13 (f 1 2 3))
+(assert-eqv "(f 1 3 2)" 13 (f 1 3 2))
+(assert-eqv "(f 2 1 3)" 13 (f 2 1 3))
+(assert-eqv "(f 2 3 1)" 13 (f 2 3 1))
+(assert-eqv "(f 3 1 2)" 13 (f 3 1 2))
+(assert-eqv "(f 3 2 1)" 13 (f 3 2 1))
