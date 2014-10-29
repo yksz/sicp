a9b70ce8faaa4e5ee5eec34bf42093f378422944
yksz <user4git@gmail.com>
2014-10-29 17:37:33 +0900

    Add 1-16.scm

---
+++ ch1/1-16.scm
@@ -0,0 +1,21 @@
+(define (fast-expt b n)
+  (define (fast-expt-iter b n a)
+    (cond ((= n 0) a)
+          ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+          (else (fast-expt-iter b (- n 1) (* a b)))))
+  (fast-expt-iter b n 1))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (square x)
+  (* x x))
+
+
+(require "../util/assert")
+(assert-eqv "(fast-expt 2 1)"     2 (fast-expt 2 1))
+(assert-eqv "(fast-expt 2 2)"     4 (fast-expt 2 2))
+(assert-eqv "(fast-expt 2 4)"    16 (fast-expt 2 4))
+(assert-eqv "(fast-expt 2 6)"    64 (fast-expt 2 6))
+(assert-eqv "(fast-expt 2 8)"   256 (fast-expt 2 8))
+(assert-eqv "(fast-expt 2 10)" 1024 (fast-expt 2 10))
