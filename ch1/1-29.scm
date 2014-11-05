bf1917a35f7c036b549e177ca3a4459436500d2b
yksz <user4git@gmail.com>
2014-11-05 17:57:08 +0900

    Add 1-29.scm

---
+++ ch1/1-29.scm
@@ -0,0 +1,32 @@
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+(define (simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (y k)
+    (f (+ a (* k h))))
+  (define (term k)
+    (cond ((= k 0) (y k))
+          ((= k n) (y k))
+          ((odd? k) (* 4 (y k)))
+          (else (* 2 (y k)))))
+  (define (next x)
+    (+ x 1))
+  (exact->inexact (* (/ h 3) (sum term 0 next n))))
+
+(define (cube x)
+  (* x x x))
+
+
+(require "../util/assert")
+(assert-approximate "(integral cube 0 1 0.01)" 0.25 (integral cube 0 1 0.01) 0.01)
+(assert-eqv "(simpson cube 0 1 100)"  0.25 (simpson cube 0 1 100))
+(assert-eqv "(simpson cube 0 1 1000)" 0.25 (simpson cube 0 1 1000))
