29cabb9ee1db48469fa79a5ee5395a2fafe6956f
yksz <user4git@gmail.com>
2014-11-05 12:02:48 +0900

    Add 1-21.scm

---
+++ ch1/1-21.scm
@@ -0,0 +1,19 @@
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (square x)
+  (* x x))
+
+
+(require "../util/assert")
+(assert-eqv "(smallest-divisor 199)"    199 (smallest-divisor 199))
+(assert-eqv "(smallest-divisor 1999)"  1999 (smallest-divisor 1999))
+(assert-eqv "(smallest-divisor 19999)"    7 (smallest-divisor 19999))
