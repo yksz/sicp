8fce4779568f6090a5e1def8941acf47a94c73e3
yksz <user4git@gmail.com>
2014-11-06 18:01:47 +0900

    Add 1-31.scm

---
+++ ch1/1-31.scm
@@ -0,0 +1,26 @@
+;; recursive process
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a) next b))))
+
+;; iterative process
+(define (product term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* result (term a)))))
+  (iter a 1))
+
+(define (pi n)
+  (define (square x) (* x x))
+  (define (next x) (+ x 2))
+  (define (term x)
+    (/ (* x (next x))
+       (square (+ x 1))))
+  (* 4 (product term 2.0 next n)))
+
+
+(require "../util/assert")
+(assert-approximate "(pi 1000)" 3.141592 (pi 1000) 0.01)
