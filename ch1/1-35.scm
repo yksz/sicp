3732468db7f84084a7191f77eb44401c5f6ee165
yksz <user4git@gmail.com>
2014-11-07 18:03:36 +0900

    Add 1-35.scm

---
+++ ch1/1-35.scm
@@ -0,0 +1,21 @@
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (golden-ratio)
+  (fixed-point (lambda (x) (+ 1 (/ 1 x)))
+               1.0))
+
+(define gloden-ratio (/ (+ 1 (sqrt 5) 2)))
+
+
+(require "../util/assert")
+(assert-approximate "(golden-ratio)" (/ (+ 1 (sqrt 5)) 2) (golden-ratio) 0.00001)
