125b44e5741f519d98f87044aa7f8543a8695f57
yksz <user4git@gmail.com>
2014-11-10 17:37:17 +0900

    Add 1-43.scm

---
+++ ch1/1-43.scm
@@ -0,0 +1,15 @@
+(define (repeated f n)
+  (if (= n 1)
+    (lambda (x)
+      (f x))
+    (compose f (repeated f (- n 1)))))
+
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+
+(define (square x) (* x x))
+
+
+(require "../util/assert")
+(assert-eqv "((repeated square 2) 5)" 625 ((repeated square 2) 5))
