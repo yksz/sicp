7b9c3984fe870879341eaf00daf7e2b49e50e8e1
yksz <user4git@gmail.com>
2014-11-10 12:59:29 +0900

    Add 1-39.scm

---
+++ ch1/1-39.scm
@@ -0,0 +1,22 @@
+(define (cont-frac n d k)
+  (define (recur i)
+    (if (> i k)
+        0
+        (/ (n i)
+           (+ (d i) (recur (+ i 1))))))
+  (recur 1))
+
+(define (tan-cf x k)
+  (cont-frac (lambda (i)
+               (if (= i 1)
+                   x
+                   (* -1 x x)))
+             (lambda (i)
+               (- (* 2 i) 1))
+             k))
+
+(define pi 3.14159)
+
+
+(require "../util/assert")
+(assert-approximate "(tan-cf (/ pi 4) 10)" 1.0 (tan-cf (/ pi 4) 10) 0.0001)
