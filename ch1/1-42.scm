b3031d4146563e71abb7f8ec3eb395a4b63724fb
yksz <user4git@gmail.com>
2014-11-10 17:36:54 +0900

    Add 1-42.scm

---
+++ ch1/1-42.scm
@@ -0,0 +1,10 @@
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+
+(define (square x) (* x x))
+(define (inc x) (+ x 1))
+
+
+(require "../util/assert")
+(assert-eqv "((compose square inc) 6)" 49 ((compose square inc) 6))
