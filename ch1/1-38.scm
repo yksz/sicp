9998aabb12fb0070876b48fb9f4e258084fe7d65
yksz <user4git@gmail.com>
2014-11-10 12:55:24 +0900

    Add 1-38.scm

---
+++ ch1/1-38.scm
@@ -0,0 +1,22 @@
+(define (cont-frac n d k)
+  (define (recur i)
+    (if (> i k)
+        0
+        (/ (n i)
+           (+ (d i) (recur (+ i 1))))))
+  (recur 1))
+
+(define (e k)
+  (+ (cont-frac (lambda (i) 1.0)
+                (lambda (i)
+                  (if (= (remainder (- i 2) 3) 0)
+                      (* 2
+                         (+ (/ (- i 2) 3)
+                            1))
+                      1))
+                k)
+     2))
+
+
+(require "../util/assert")
+(assert-approximate "(e 10)" 2.71828 (e 10) 0.0001)
