9b59f6c3015c5ec4310dcd8735986bc394be20a0
yksz <user4git@gmail.com>
2014-11-17 13:28:09 +0900

    Add 2-19.scm

---
+++ ch2/2-19.scm
@@ -0,0 +1,23 @@
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+
+(require "../util/assert")
+(define us-coins (list 50 25 10 5 1))
+(assert-eqv "(cc 100 us-coins)" 292 (cc 100 us-coins))
