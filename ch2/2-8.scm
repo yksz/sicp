2747b4fbb2d9659eaf21b3368ea60f85aebc938c
yksz <user4git@gmail.com>
2014-11-17 13:27:17 +0900

    Add 2-8.scm

---
+++ ch2/2-8.scm
@@ -0,0 +1,18 @@
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound i)
+  (max (car i) (cdr i)))
+
+(define (lower-bound i)
+  (min (car i) (cdr i)))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+
+(require "../util/assert")
+(define x (make-interval 8 4))
+(define y (make-interval 2 0))
+(assert-eqv "(upper-bound (sub-interval x y))" 8 (upper-bound (sub-interval x y)))
+(assert-eqv "(lower-bound (sub-interval x y))" 2 (lower-bound (sub-interval x y)))
