1c8245b9b87f9a5235060af763e11e5c2ab333e1
yksz <user4git@gmail.com>
2014-11-17 13:28:37 +0900

    Add 2-22.scm

---
+++ ch2/2-22.scm
@@ -0,0 +1,16 @@
+(define nil ())
+
+(define (square x) (* x x))
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (append answer
+                      (list (square (car things)))))))
+  (iter items nil))
+
+
+(require "../util/assert")
+(assert-equal "(square-list (list 1 2 3 4))" (list 1 4 9 16) (square-list (list 1 2 3 4)))
