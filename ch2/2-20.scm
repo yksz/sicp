42b198000d0813503f0dd548fa0b34254026f834
yksz <user4git@gmail.com>
2014-11-17 13:28:20 +0900

    Add 2-20.scm

---
+++ ch2/2-20.scm
@@ -0,0 +1,14 @@
+(define (same-parity first . rest)
+  (let ((filter? (if (odd? first) odd? even?)))
+    (define (iter src dst)
+      (if (null? src)
+          dst
+          (if (filter? (car src))
+              (iter (cdr src) (append dst (list (car src))))
+              (iter (cdr src) dst))))
+    (iter rest (list first))))
+
+
+(require "../util/assert")
+(assert-equal "(same-parity 1 2 3 4 5 6 7)" (list 1 3 5 7) (same-parity 1 2 3 4 5 6 7))
+(assert-equal "(same-parity 2 3 4 5 6 7)" (list 2 4 6) (same-parity 2 3 4 5 6 7))
