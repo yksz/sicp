443b32193ba26af562814420d2869e92522f3e4f
yksz <user4git@gmail.com>
2014-11-19 18:12:54 +0900

    Add 2.25.scm

---
+++ ch2/2.25.scm
@@ -0,0 +1,10 @@
+(require "../util/assert")
+
+(define items1 '(1 3 (5 7) 9))
+(assert-eqv "items1" 7 (car (cdr (car (cdr (cdr items1))))))
+
+(define items2 '((7)))
+(assert-eqv "items2" 7 (car (car items2)))
+
+(define items3 '(1 (2 (3 (4 (5 (6 7)))))))
+(assert-eqv "items3" 7 (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr items3)))))))))))))
