f80f07d0a8e64f4809f51ccddc3306f991f3acb0
yksz <user4git@gmail.com>
2014-11-27 17:53:25 +0900

    Add 2.30.scm

---
+++ ch2/2.30.scm
@@ -0,0 +1,13 @@
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree sub-tree)
+             (square sub-tree)))
+       tree))
+
+(define (square x) (* x x))
+
+
+(require "../util/assert")
+(define x '(1 (2 (3 4) 5) (6 7)))
+(assert-equal "(square-tree x)" '(1 (4 (9 16) 25) (36 49)) (square-tree x))
