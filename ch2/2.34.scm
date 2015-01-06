1a3d3d75a5b059ede346068d69a0a694fbea6553
yksz <user4git@gmail.com>
2015-01-06 15:16:58 +0900

    Add 2.34.scm

---
+++ ch2/2.34.scm
@@ -0,0 +1,16 @@
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ this-coeff (* higher-terms x)))
+              0
+              coefficient-sequence))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+
+(require "../util/assert")
+;; 1 + 3x + 5x^3 + x^5 at x = 2
+(assert-eqv "(horner-eval 2 (list 1 3 0 5 0 1)" 79 (horner-eval 2 (list 1 3 0 5 0 1)))
