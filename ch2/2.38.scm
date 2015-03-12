ce040011b55867651e0dbaf411af860cb05d3054
yksz <user4git@gmail.com>
2015-03-12 11:32:11 +0900

    Add 2.38.scm

---
+++ ch2/2.38.scm
@@ -0,0 +1,27 @@
+(define (fold-right op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (fold-right op initial (cdr sequence)))))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define nil '())
+
+(fold-right / 1 (list 1 2 3))
+; => 3/2
+
+(fold-left / 1 (list 1 2 3))
+; => 1/6
+
+(fold-right list nil (list 1 2 3))
+; => (1 (2 (3 ())))
+
+(fold-left list nil (list 1 2 3))
+; => (((() 1) 2) 3)
